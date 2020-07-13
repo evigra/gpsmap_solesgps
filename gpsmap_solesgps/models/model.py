f41380bb51675aaab1ba0e22fd77ff974e3b157a
Eduardo Vizcaino <evigra@hotmail.com>
2020-07-12 22:15:18 -0500

    Update

---
+++ gpsmap_solesgps/models/model.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import datetime
+import requests
+import random
+from dateutil.relativedelta import relativedelta
+from odoo import api, fields, models, _
+
+class positions(models.Model):
+    _inherit = "gpsmap.positions"
+    
+
