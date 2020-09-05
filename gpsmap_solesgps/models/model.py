e48d0469818b83d8369f0f984995d8c8c2bc6ca2
Eduardo Vizcaino <evigra@hotmail.com>
2020-09-05 09:07:42 -0500

    Update

--- gpsmap_solesgps/models/model.py
+++ gpsmap_solesgps/models/model.py
@@ -14,7 +14,7 @@ class vehicle(models.Model):
     _inherit = "fleet.vehicle"
     recargado                                   = fields.Datetime('Recargado')
     def run_scheduler_recarga(self):
-        vehicle_args                            =[]        
+        vehicle_args                            =[('devicetime', '>=', (context_today()-datetime.timedelta(days=10)).strftime('%Y-%m-%d'))]        
         return_positions                        ={}
         vehicle_data                            =self.search(vehicle_args, offset=0, limit=None, order=None)
 
