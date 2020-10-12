68fb02a7f242a2dbf1e7970de358fcf2d8630b9b
[SolesGPS] Eduardo Vizcaino <evigra@hotmail.com>
2020-10-12 11:39:31 -0500

    Update

--- gpsmap_solesgps/models/model.py
+++ gpsmap_solesgps/models/model.py
@@ -25,7 +25,7 @@ class vehicle(models.Model):
         for vehicle in vehicle_data:
             print("# VEHICLE =============",vehicle["name"])
             #if((vehicle["recargado"]=="" OR vehicle["recargado"]==NULL)):        
-            if(vehicle["recargado"] not in {"",Null,False}):        
+            if(vehicle["recargado"] not in {"",False}):        
                 ahora = datetime.datetime.utcnow()
                 ayer = ahora - datetime.timedelta(days=25)
                 #ayer2 = vehicle["recargado"] - datetime.timedelta(days=25)                
